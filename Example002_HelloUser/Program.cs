7e8cdcaff327c6062955d7bc889f9683cd76624a
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-18 16:14:21 +0500

    Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите Ваше имя ");
+        string username = Console.ReadLine(); //В переменную username будем сохранять имя пользователя
+        Console.WriteLine("Привет, ");
+        Console.WriteLine(username);
+    }
+}
