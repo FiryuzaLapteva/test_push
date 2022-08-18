930e487e6fb2377ea46dcb8e36fe6632b3787cf4
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-18 23:51:29 +0500

    Exampel004_Div

---
+++ Example002_HelloUser/Exampel004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        double numberA = 12;
+        double numberB = 8;
+        Console.WriteLine(numberA / numberB);
+    }
+}
