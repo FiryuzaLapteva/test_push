5a26472f8bd87d0d5416d69bfe9bc3c14d15b3ae
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-18 16:31:06 +0500

    .\Example003_sum

---
+++ Example002_HelloUser/Example003_sum/Program.cs
@@ -0,0 +1,9 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberA = 8;
+        int numberB = 9;
+        Console.WriteLine(numberA + numberB);
+    }
+}
