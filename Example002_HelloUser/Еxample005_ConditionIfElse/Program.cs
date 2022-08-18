7ade84ff9ae342ad367e9ed28f8c361eac7e054f
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-19 00:16:26 +0500

    Example005

---
+++ Example002_HelloUser/Еxample005_ConditionIfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите имя пользователя");
+        string username = Console.ReadLine();
+        if (username.ToLower() == "Маша")
+        {
+            Console.WriteLine("Ура, опять Маша");
+        }
+        else
+        {
+            Console.WriteLine("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
+}
